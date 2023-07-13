a7ea7a33382dd72691bf5f2433dbc79f64109a57
Rahul Dange <rahuldangeofficial@gmail.com>
2023-07-13 19:08:31 +0530

    Add firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
 import "./App.css";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVJaEnRwkFqfSh6QHlLquJG9oGDAVCMH8",
+  authDomain: "trd-fb-00.firebaseapp.com",
+  projectId: "trd-fb-00",
+  storageBucket: "trd-fb-00.appspot.com",
+  messagingSenderId: "562347353539",
+  appId: "1:562347353539:web:0be76a5d8ceca51a3c0976"
+};
+
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
